d739ac377bd61505f0f546fbfb27dbb29c2323d0
GitHub Action <action@github.com>
2024-03-19 04:54:02 +0000

    Add executable built by GitHub Action

---
+++ build/CMakeFiles/tree.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tree.dir/main.c.o"
+  "CMakeFiles/tree.dir/main.c.o.d"
+  "CMakeFiles/tree.dir/tree.c.o"
+  "CMakeFiles/tree.dir/tree.c.o.d"
+  "tree"
+  "tree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
